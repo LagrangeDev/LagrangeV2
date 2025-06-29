fa9264c1cc630736247f24c009254a14832ca726
Xeron <2398003522@qq.com>
2025-06-29 17:01:03 +0800

    [Core] Implemented group rename

---
+++ Lagrange.Core/Internal/Packets/Service/Oidb_0x89A.cs
@@ -0,0 +1,22 @@
+﻿using Lagrange.Proto;
+
+namespace Lagrange.Core.Internal.Packets.Service;
+
+#pragma warning disable CS8618
+
+[ProtoPackable]
+internal partial class GroupRenameReqBody
+{
+    [ProtoMember(3)] public string TargetName { get; set; }
+}
+
+[ProtoPackable]
+internal partial class D89AReqBody
+{
+    [ProtoMember(1)] public long GroupUin { get; set; }
+
+    [ProtoMember(2)] public GroupRenameReqBody Body { get; set; }
+}
+
+[ProtoPackable]
+internal partial class D89ARspBody;
