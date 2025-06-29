4dbcb11770801555d98b60bd144b8ba17b9e6949
Xeron <2398003522@qq.com>
2025-06-29 17:01:09 +0800

    [Milky] Implemented `set_group_name`

---
+++ Lagrange.Milky/Api/Handler/Group/SetGroupNameHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+using Lagrange.Core;
+using Lagrange.Core.Common.Interface;
+
+namespace Lagrange.Milky.Api.Handler.Group;
+
+[Api("set_group_name")]
+public class SetGroupNameHandler(BotContext bot) : IEmptyResultApiHandler<SetGroupNameParameter>
+{
+    public async Task HandleAsync(SetGroupNameParameter parameter, CancellationToken token)
+    {
+        await bot.GroupRename(parameter.GroupId, parameter.Name);
+    }
+}
+
+public class SetGroupNameParameter(long groupId, string name)
+{
+    [JsonRequired]
+    [JsonPropertyName("group_id")]
+    public long GroupId { get; init; } = groupId;
+
+    [JsonRequired]
+    [JsonPropertyName("name")]
+    public string Name { get; init; } = name;
+}
