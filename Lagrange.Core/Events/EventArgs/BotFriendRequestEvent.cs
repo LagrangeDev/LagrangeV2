ff5e4a88fd7f3cd8f27dc7964bfc0d366c5b22c7
Szzrain <szzrain@szzrain.com>
2025-08-18 10:32:25 +0800

    [Core & Milky] Implement `friend_request` event

---
+++ Lagrange.Core/Events/EventArgs/BotFriendRequestEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace Lagrange.Core.Events.EventArgs;
+
+public class BotFriendRequestEvent(string initiatorUid, long initiatorUin, string message, string source) : EventBase
+{
+    public string InitiatorUid { get; } = initiatorUid;
+
+    public long InitiatorUin { get; } = initiatorUin;
+    
+    public string Message { get; } = message;
+    
+    public string Source { get; } = source;
+
+    public override string ToEventMessage()
+    {
+        return $"{nameof(BotFriendRequestEvent)}: RequestId: {InitiatorUid}, " +
+               $"InitiatorUin: {InitiatorUin}, " +
+               $"Message: {Message}, " +
+               $"Source: {Source}";
+    }
+}
