8892d15ac169bc522045e86b3da46df9ecd3c7ea
DarkRRb <177549718+DarkRRb@users.noreply.github.com>
2026-04-10 15:33:46 +0800

    [Milky] Fix DateTime to timestamp conversion exception in specific scenarios

--- Lagrange.Milky/Extension/DateTimeExtension.cs
+++ Lagrange.Milky/Extension/DateTimeExtension.cs
@@ -3,4 +3,6 @@ namespace Lagrange.Milky.Extension;
 public static class DateTimeExtension
 {
     public static long ToUnixTimeSeconds(this DateTime time) => new DateTimeOffset(time, TimeSpan.Zero).ToUnixTimeSeconds();
+
+    public static long LocalTimeToUnixTimeSeconds(this DateTime time) => new DateTimeOffset(time).ToUnixTimeSeconds();
 }
